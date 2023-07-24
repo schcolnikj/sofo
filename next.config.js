d026ba1a383dd8da72d1f3010a0e4d2dedd825e6
Juan Schcolnik <schcolnikj@gmail.com>
2023-07-24 15:01:58 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
         domains: ['ar.freddo.com', "schockba.com.ar", "recetuquis.com", "img-global.cpcdn.com", "www.gastrolabweb.com", "badun.nestle.es", "encrypted-tbn0.gstatic.com", "i.ytimg.com", "www.piloncilloyvainilla.com", "t1.uc.ltmcdn.com"]
     },
     typescript: {
-        ignoreBuildErrors: true;
-    }
+        ignoreBuildErrors: true
+    },
 }
 
 module.exports = nextConfig
